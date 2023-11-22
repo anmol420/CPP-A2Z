940b60f682b0dbadfabb5d1ac78782ef4b7c04b6
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-11-23 01:59:20 +0530

    First Commit

---
+++ Input_Output/input_ouput.cpp
@@ -0,0 +1,11 @@
+#include "input_ouput.h"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin >> x >> y;
+    cout << "Value Of x: " << x << " and y: " << y << endl;
+    return 0;
+}
