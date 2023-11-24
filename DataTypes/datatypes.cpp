024c9e53028d30ee796e5f71602e193efa70d544
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-11-24 18:09:23 +0530

    Added Datatypes

---
+++ DataTypes/datatypes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // integer type
+    int x=10;
+    // long type
+    long x1=10;
+    // long long type [for very large numbers]
+    long long x2=10000000000;
+
+    // float / double type [for decimal numbers]
+    float x3=4.33;
+    double x4=233.33;
+
+    // string and getline [for spaced strings] type
+    string s1;
+    getline(cin,s1);
+    cout << s1;
+
+    // character type
+    char ch='x';
+
+    return 0;
+}
